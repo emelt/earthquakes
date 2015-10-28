53f461da40919f68fe594f7a8fa4c59c1acffca3
Emel Topaloglu <Emel.Topaloglu@monitise.com>
2015-10-28 21:54:53 +0200

    Initial Commit

---
+++ SonDepremler/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SonDepremler
+//
+//  Created by Emel Topaloglu on 28/10/2015.
+//  Copyright © 2015 Emel Topaloglu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
